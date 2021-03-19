92563eeee6c9f6f6f6845f94fa366f18a1b1273a
Suren <kat025@mail.ru>
2021-03-19 23:15:22 +0400

    Set login logic functionality and fs packege

--- src/screens/Login/SignIn.js
+++ src/screens/Login/SignIn.js
@@ -1,5 +1,5 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   View,
   Text,
@@ -7,24 +7,65 @@ import {
   TextInput,
   SafeAreaView,
   TouchableOpacity,
+  Alert,
 } from 'react-native';
 import Button from '../../components/ui/Button';
 
 import {GRAY_LIGHT, WHITE} from '../../constants/styles';
+import {login} from '../../../utils/helper';
 
 const SignIn = ({navigation}) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    reset();
+  }, []);
+
+  const reset = () => {
+    setEmail('');
+    setPassword('');
+  };
+
+  const signIn = async () => {
+    console.log('emal pass - ', email, password);
+    if (!email || !password) {
+      Alert.alert('Fill all fields', '', [{text: 'OK', onPress: () => {}}]);
+    } else {
+      let data = await login(email, password);
+      console.log('login data - ', data);
+      if (data) {
+        navigation.navigate('Home', {data: data});
+        reset();
+      } else {
+        Alert.alert('Invalid email or password', 'Try again', [
+          {text: 'ok', onPress: () => reset()},
+        ]);
+      }
+    }
+  };
+
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: WHITE}}>
       <View style={styles.main}>
         <View style={styles.container}>
           <Text style={styles.titleText}>Welome Back!</Text>
-          <TextInput style={styles.textInput} placeholder="Email Address" />
+          <TextInput
+            style={styles.textInput}
+            placeholder="Email Address"
+            value={email}
+            onChangeText={(val) => setEmail(val.toLowerCase())}
+            autoCapitalize="none"
+          />
           <TextInput
             style={[styles.textInput, {marginTop: 12}]}
+            value={password}
+            onChangeText={(val) => setPassword(val)}
             placeholder="Password"
             secureTextEntry
+            autoCapitalize="none"
           />
-          <Button title="SIGN IN" onPress={() => navigation.navigate('Home')} />
+          <Button title="SIGN IN" onPress={signIn} />
         </View>
 
         <View style={styles.bottomView}>
